1d11fd25f04ca873584383db66b813847ea14b9d
esimms311 <eric.simmons26@gmail.com>
2017-01-25 10:55:14 -0700

    starting over on notes section

--- public/js/services/notesFactory.js
+++ public/js/services/notesFactory.js
@@ -1,5 +1,7 @@
 angular.module('clickEvent').factory('notesFactory', function() {
-  return {
+
+
+return {
     put: function(note) {
       localStorage.setItem('note' + note.id, JSON.stringify(note));
       return this.getAll();
@@ -9,7 +11,7 @@ angular.module('clickEvent').factory('notesFactory', function() {
     },
     getLastNote: function(){
       var lastNote = [];
-      for (var i = 0; i &lt; localStorage.length; i++) {
+      for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i).indexOf('note') !== -1) {
           var note = localStorage.getItem(localStorage.key(i));
           lastNote = JSON.parse(note);
@@ -19,7 +21,7 @@ angular.module('clickEvent').factory('notesFactory', function() {
     },
     getAll: function() {
       var notes = [];
-      for (var i = 0; i &lt; localStorage.length; i++) {
+      for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i).indexOf('note') !== -1) {
           var note = localStorage.getItem(localStorage.key(i));
           notes.push(JSON.parse(note));
@@ -28,7 +30,7 @@ angular.module('clickEvent').factory('notesFactory', function() {
       return notes;
     },
     deleteById: function(index){
-      for (var i = 0; i &lt; localStorage.length; i++) {
+      for (var i = 0; i < localStorage.length; i++) {
           if(localStorage.key(i) == 'note'+index)
             {
               localStorage.removeItem(localStorage.key(i));
@@ -39,3 +41,44 @@ angular.module('clickEvent').factory('notesFactory', function() {
     }
   };
 });
+// angular.module('clickEvent').factory('notesFactory', function() {
+//   return {
+//     put: function(note) {
+//       localStorage.setItem('note' + note.id, JSON.stringify(note));
+//       return this.getAll();
+//     },
+//     get: function(index) {
+//       return JSON.parse(localStorage.getItem('note' + index));
+//     },
+//     getLastNote: function(){
+//       var lastNote = [];
+//       for (var i = 0; i &lt; localStorage.length; i++) {
+//         if (localStorage.key(i).indexOf('note') !== -1) {
+//           var note = localStorage.getItem(localStorage.key(i));
+//           lastNote = JSON.parse(note);
+//         }
+//       }
+//       return lastNote;
+//     },
+//     getAll: function() {
+//       var notes = [];
+//       for (var i = 0; i &lt; localStorage.length; i++) {
+//         if (localStorage.key(i).indexOf('note') !== -1) {
+//           var note = localStorage.getItem(localStorage.key(i));
+//           notes.push(JSON.parse(note));
+//         }
+//       }
+//       return notes;
+//     },
+//     deleteById: function(index){
+//       for (var i = 0; i &lt; localStorage.length; i++) {
+//           if(localStorage.key(i) == 'note'+index)
+//             {
+//               localStorage.removeItem(localStorage.key(i));
+//               break;
+//             }
+//       }
+//       return this.getAll();
+//     }
+//   };
+// });
